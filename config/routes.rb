a9aa4b9ed0479d770d5e9a8bb587adb8a1ad4f8a
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-09-05 13:13:42 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   resources :posts, only: [:index, :show, :new, :create, :edit]
-=======
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
->>>>>>> 8c33f51346ac72826f07686cb4befd1ee3914963
   get '/posts/:id/body', to: 'posts#body'
 
 end
